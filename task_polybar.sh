0222dce706241ec0401b2f710a8d8bd4078e988c
Victor <victorabenilfernandez@gmail.com>
2019-08-23 15:15:05 +0300

    Add reference to orginal github repository

--- task_polybar.sh
+++ task_polybar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Originial script taken from https://github.com/dakuten/taskwarrior-polybar
 most_urgent_desc=`task rc.verbose: rc.report.next.columns:description rc.report.next.labels:1 limit:1 next`
 most_urgent_id=`task rc.verbose: rc.report.next.columns:id rc.report.next.labels:1 limit:1 next`
 most_urgent_due=`task rc.verbose: rc.report.next.columns:due.relative rc.report.next.labels:1 limit:1 next`
