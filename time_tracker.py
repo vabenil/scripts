71fdc53af3ad1ab7e2bf0909d96797deb180cbe6
Victor <victorabenilfernandez@gmail.com>
2019-08-29 17:15:39 +0300

    Being more PEP 8 friendly

--- time_tracker.py
+++ time_tracker.py
@@ -37,21 +37,23 @@ class TimeTracker:
 
         if self.lfocused is not None and self.lfocused.type == 'con':
             if self.lfocused_class not in self.focus_history.keys():
-                self.focus_history[ self.lfocused_class ] = {
+                self.focus_history[self.lfocused_class] = {
                     'time_spent': current_time - self.time_since_lfocus,
                     'window': self.lfocused
                 }
             else:
-                self.focus_history[ self.lfocused_class ]['time_spent'] += current_time - self.time_since_lfocus
+                last_time_spent = current_time - self.time_since_lfocus
+                self.focus_history[self.lfocused_class]['time_spent'] += last_time_spent
 
         if self.focused.type == 'con':
             if self.focused_class not in self.focus_history.keys():
-                self.focus_history[ self.focused_class ] = {
+                self.focus_history[self.focused_class] = {
                     'time_spent': 0,
                     'window': self.focused
                 }
             else:
-                self.focus_history[ self.focused_class ]['time_spent'] += current_time - self.time_since_lfocus
+                last_time_spent = current_time - self.time_since_lfocus
+                self.focus_history[self.focused_class]['time_spent'] += last_time_spent
 
         self.time_since_lfocus = time.perf_counter()
 
@@ -65,17 +67,17 @@ class TimeTracker:
         self.save_to_history()
         self.notification_manager()
 
-        print( "%s focused" % self.focused_class )
+        print("%s focused" % self.focused_class)
 
 
     def notificate(self):
         window_class = self.focused_class
-        time_spent = self.focus_history[ self.focused_class ]['time_spent']
+        time_spent = self.focus_history[self.focused_class]['time_spent']
 
-        notification = "You have spent %s seconds in %s" % ( int(time_spent) + self.delay, window_class )
+        notification = "You have spent %s seconds in %s" % (int(time_spent) + self.delay, window_class)
 
-        print( notification )
-        print( "time_spent: %d" % int(time_spent) )
+        print(notification)
+        print("time_spent: %d" % int(time_spent))
 
         subprocess.run(['notify-send', notification])
 
@@ -88,14 +90,15 @@ class TimeTracker:
         if self.focused.type != 'con':
             thr = pending_notification['thread']
 
-            thr.cancel() if thr.is_alive() else 0
+            if thr.is_alive():
+                thr.cancel()
 
             return 0
 
-        time_spent = int(self.focus_history[ self.focused_class ]['time_spent']) 
+        time_spent = int(self.focus_history[self.focused_class]['time_spent']) 
         sec_to_wait = int( time_spent / self.delay + 1) * self.delay - int(time_spent)
             
-        print( "time_spent: %d, sec_to_wait: %d" % ( time_spent, sec_to_wait )  )
+        print("time_spent: %d, sec_to_wait: %d" % (time_spent, sec_to_wait))
         # Create new pending notification
         if not self.pending_notification:
             self.pending_notification['name'] = self.focused_class
@@ -106,7 +109,6 @@ class TimeTracker:
             self.pending_notification['thread'].start()
         # Replace last pending notification with a new one
         elif self.focused_class != self.pending_notification['name']:
-            # print( "focused: %s, pending: %s" % (self.focused_class, self.pending_notification['name']) )
             thr = self.pending_notification['thread']
 
             thr.cancel() if thr.is_alive() else 0
@@ -118,7 +120,7 @@ class TimeTracker:
             )
             self.pending_notification['thread'].start()
         else:
-            print( "focused: %s, pending: %s" % (self.focused_class, self.pending_notification['name']) )
+            print("focused: %s, pending: %s" % (self.focused_class, self.pending_notification['name']))
 
 
 
