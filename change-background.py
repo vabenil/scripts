9bd9de6050ccba15e03521b212402e243e49a3ca
Victor <victorabenilfernandez@gmail.com>
2019-09-14 00:05:20 +0300

    Added --random flag

--- change-background.py
+++ change-background.py
@@ -3,30 +3,31 @@ import os
 import sys
 import argparse
 import subprocess
+import random
 
 
 # File to save wallpapers
 BG_REG_FILE_PATH = "/tmp/current-bg"
 
-def save_bg( bg ):
+def save_bg(bg):
     try:
-        bg_reg_file = open( BG_REG_FILE_PATH, "w" )
-        bg_reg_file.write( bg )
+        bg_reg_file = open(BG_REG_FILE_PATH, "w")
+        bg_reg_file.write(bg)
 
         bg_reg_file.close()
     except:
-        print( "Couldn't write to file %s" % BG_REG_FILE_PATH )
+        print("Couldn't write to file %s" % BG_REG_FILE_PATH)
     pass
 
-def get_current_bg():
+def get_bg():
     bg = None
     try:
-        bg_reg_file = open( BG_REG_FILE_PATH, "r" )
+        bg_reg_file = open(BG_REG_FILE_PATH, "r")
     except FileNotFoundError:
         pass
     except Exception as e:
-        print( "Error while opening file %s" % BG_REG_FILE_PATH )
-        exit( 1 )
+        print("Error while opening file %s" % BG_REG_FILE_PATH)
+        exit(1)
     else:
         bg = bg_reg_file.read() 
 
@@ -34,20 +35,55 @@ def get_current_bg():
 
     return bg
 
-def set_bg( bg_path ):
+def set_bg(bg_path):
     subprocess.run(['feh', '--bg-fill', '--no-fehbg', bg_path])
 
+def random_bg():
+    bgs = sorted([
+        os.path.join(bg_dir, f) for f in os.listdir(bg_dir) \
+                if os.path.isfile( os.path.join(bg_dir, f)) and f.endswith(('.jpg', '.jpeg', '.png', '.bmp'))
+    ])
+    bg_path = get_bg()
+
+    bg_index = bgs.index(bg_path) if bg_path in bgs else -1
+
+    if len(bgs) > 1 and bg_index != -1:
+        rand_bg = random.choice(bgs)
+        while rand_bg == bgs[bg_index]:
+            rand_bg = random.choice(bgs)
+
+        bg = rand_bg
+    else:
+        bg = bgs[0]
+
+    save_bg(bg)
+    set_bg(bg)
+
+
+def next_bg():
+    bgs = sorted([
+        os.path.join(bg_dir, f) for f in os.listdir(bg_dir) \
+                if os.path.isfile( os.path.join(bg_dir, f)) and f.endswith(('.jpg', '.jpeg', '.png', '.bmp'))
+    ])
+
+    bg_path = get_bg()
+
+    bg_index = bgs.index(bg_path) if bg_path in bgs else (len(bgs) - 1)
+
+    bg = bgs[ (bg_index + 1) % len(bgs) ]
+
+    save_bg(bg)
+    set_bg(bg)
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Traverse wallpapers in directory")
+    parser.add_argument('--random', action='store_true', help="Set file as background")
 
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f', '--filename', type=str,
-            help="Set file as background" )
-    group.add_argument('dir', nargs='?',
-            help="Wallpaper directory")
+    group.add_argument('-f', '--filename', type=str)
+    group.add_argument('dir', nargs='?', help="Wallpaper directory")
 
     args = parser.parse_args()
 
@@ -55,36 +91,27 @@ if __name__ == '__main__':
         if not os.path.isfile(args.filename):
             print("%s file doesn't exist" % args.filename)
             parser.print_help()
-            exit( 1 )
+            exit(1)
         else:
             bg_path = args.filename
 
-            if bg_path.endswith(('.jpg', '.jpeg', '.png', '.bmp')):
-                save_bg( bg_path )
-                set_bg( bg_path )
+            if bg_path.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp')):
+                save_bg(bg_path)
+                set_bg(bg_path)
             else:
-                print( "File must be an image" )
+                print("File must be an image")
                 parser.print_help()
-                exit( 1 )
+                exit(1)
     else:
         bg_dir = args.dir
 
         if not os.path.isdir(bg_dir):
-            print( "%s is not a directory" % bg_dir )
+            print("%s is not a directory" % bg_dir)
             parser.print_help()
-            exit( 1 )
+            exit(1)
 
+        if args.random:
+            random_bg()
+        else:
+            next_bg()
 
-        backgrounds = sorted([
-            os.path.join(bg_dir, f) for f in os.listdir(bg_dir) \
-                    if os.path.isfile( os.path.join(bg_dir, f)) and f.endswith(('.jpg', '.jpeg', '.png', '.bmp'))
-        ])
-
-        current_bg_path = get_current_bg()
-
-        current_bg_index = backgrounds.index(current_bg_path) if current_bg_path in backgrounds else (len(backgrounds) - 1)
-        
-        next_bg = backgrounds[ (current_bg_index + 1) % len( backgrounds ) ]
-        
-        save_bg( next_bg )
-        set_bg( next_bg )
